cd4363bf888f98ced26a0e87ebd8023b83991a8b
Frederick Cortez <frederickcortez66@gmail.com>
2022-09-21 09:01:59 -0500

    2nd commit;

--- api/server.js
+++ api/server.js
@@ -1,7 +1,10 @@
 // implement your server here
-const express = require('express')
+const express = require('express');
+const postsRouter = require('./posts/posts-router');
 const server = express();
-// require your posts router and connect it here
 
+// require your posts router and connect it here
+server.use(express.json());
+server.use('/api/posts', postsRouter);
 
 module.exports = server;
